aeba28e165929f80c4a8ac94d05773417691b2ee
Edward Naidoo <securedbread@protonmail.com>
2022-12-17 15:25:30 -0800

    more

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-const inter = Inter({ subsets: ['latin'] });
+// const inter:any = inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -11,8 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="">
-        
+        <div className=" bg-slate-300"></div>
       </main>
     </>
   )
 }
+
